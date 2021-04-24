012685df8617b16475b3cfac002cf5b93bd4435a
jmir1 <jhmiramon@gmail.com>
2021-04-25 01:00:55 +0200

    rename source factory class

--- library/src/main/java/eu/kanade/tachiyomi/source/AnimeSourceFactory.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/AnimeSourceFactory.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.source
 /**
  * A factory for creating sources at runtime.
  */
-interface SourceFactory {
+interface AnimeSourceFactory {
     /**
      * Create a new copy of the sources
      * @return The created sources
