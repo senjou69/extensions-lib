c03753fe79a9cc1cb393ffc4f423758b5891c381
jmir1 <jhmiramon@gmail.com>
2021-05-02 14:06:39 +0200

    fixed another oopsie

--- library/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
+++ library/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
@@ -1,13 +1,8 @@
 package eu.kanade.tachiyomi.network
 
-import kotlinx.coroutines.suspendCancellableCoroutine
 import okhttp3.Call
-import okhttp3.Callback
 import okhttp3.Response
 import rx.Observable
-import java.io.IOException
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
 
 
 fun Call.asObservable(): Observable<Response> {
@@ -19,32 +14,5 @@ fun Call.asObservableSuccess(): Observable<Response> {
 }
 
 suspend fun Call.await(): Response {
-    return suspendCancellableCoroutine { continuation ->
-        enqueue(
-                object : Callback {
-                    override fun onResponse(call: Call, response: Response) {
-                        if (!response.isSuccessful) {
-                            continuation.resumeWithException(Exception("HTTP error ${response.code}"))
-                            return
-                        }
-
-                        continuation.resume(response)
-                    }
-
-                    override fun onFailure(call: Call, e: IOException) {
-                        // Don't bother with resuming the continuation if it is already cancelled.
-                        if (continuation.isCancelled) return
-                        continuation.resumeWithException(e)
-                    }
-                }
-        )
-
-        continuation.invokeOnCancellation {
-            try {
-                cancel()
-            } catch (ex: Throwable) {
-                // Ignore cancel exception
-            }
-        }
-    }
+    throw Exception("Stub!")
 }
