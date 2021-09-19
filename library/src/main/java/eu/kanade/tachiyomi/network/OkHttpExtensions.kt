2779f61a1ccd047d95e48a1e54cfc82bf3077513
AriaMoradi <AriaMoradi@users.noreply.github.com>
2021-09-19 22:29:29 +0200

    remove extension access to Call.await()

--- library/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
+++ library/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
@@ -4,15 +4,10 @@ import okhttp3.Call
 import okhttp3.Response
 import rx.Observable
 
-
 fun Call.asObservable(): Observable<Response> {
     throw Exception("Stub!")
 }
 
 fun Call.asObservableSuccess(): Observable<Response> {
-    throw Exception("Stub!")
-}
-
-suspend fun Call.await(): Response {
     throw Exception("Stub!")
 }
